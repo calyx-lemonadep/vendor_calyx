934cef22f3bd4cce0fdfcd6d3e86b1c7513acb33
Linux4 <tim@linux4.de>
2020-04-20 17:46:02 +0200

    calyx: add support for including dynamic sepolicy
    Change-Id: I4a91be333b1e02b89ba8da5fc418c36e012bc518

--- sepolicy/common/sepolicy.mk
+++ sepolicy/common/sepolicy.mk
@@ -3,8 +3,23 @@
 # inherit from Calyx
 #
 
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     vendor/calyx/sepolicy/common/private
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
     vendor/calyx/sepolicy/common/public
+
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    vendor/calyx/sepolicy/common/dynamic
+else
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    vendor/calyx/sepolicy/common/dynamic \
+    vendor/calyx/sepolicy/common/vendor
+endif
